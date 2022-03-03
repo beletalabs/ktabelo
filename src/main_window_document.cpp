c92a9db7dcaa379e535e1d83f1b069125f4505bf
naracanto <91744875+naracanto@users.noreply.github.com>
2022-03-03 18:29:22 +0100

    Set document's window title

--- src/main_window_document.cpp
+++ src/main_window_document.cpp
@@ -6,9 +6,13 @@
 
 #include "main_window_document.h"
 
+#include <KLocalizedString>
+
 
 MainWindowDocument::MainWindowDocument(QWidget *parent)
     : QWidget(parent)
 {
+    static int sequenceNumber = 1;
 
+    setWindowTitle(i18n("Untitled (%1)", sequenceNumber++));
 }
