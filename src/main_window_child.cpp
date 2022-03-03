8fb09ad446b1a05b422138d519bbff427bae6be0
naracanto <91744875+naracanto@users.noreply.github.com>
2022-03-03 16:09:46 +0100

    Add basic class 'MainWindowChild'

---
+++ src/main_window_child.cpp
@@ -0,0 +1,14 @@
+/*
+    SPDX-FileCopyrightText: 2022 naracanto https://naracanto.github.io
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include "main_window_child.h"
+
+
+MainWindowChild::MainWindowChild(QWidget *parent)
+    : QWidget(parent)
+{
+
+}
---
+++ src/main_window_child.h
@@ -0,0 +1,19 @@
+/*
+    SPDX-FileCopyrightText: 2022 naracanto https://naracanto.github.io
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef MAIN_WINDOW_CHILD_H
+#define MAIN_WINDOW_CHILD_H
+
+#include <QWidget>
+
+
+class MainWindowChild : public QWidget
+{
+public:
+    explicit MainWindowChild(QWidget *parent = nullptr);
+};
+
+#endif // MAIN_WINDOW_CHILD_H
