569b63b9b6e33320053f3c639b4f58c1106010a3
naracanto <91744875+naracanto@users.noreply.github.com>
2022-03-03 16:00:14 +0100

    Add basic class 'MainWindowArea'

---
+++ src/main_window_area.cpp
@@ -0,0 +1,14 @@
+/*
+    SPDX-FileCopyrightText: 2022 naracanto https://naracanto.github.io
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include "main_window_area.h"
+
+
+MainWindowArea::MainWindowArea(QWidget *parent)
+    : QMdiArea(parent)
+{
+
+}
